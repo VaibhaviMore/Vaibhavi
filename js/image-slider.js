f9fa8197ee6a4dfb39c7d9c4e619550cb0e8353b
VaibhaviMore <vaibmore26@gmail.com>
2019-03-11 12:36:32 -0400

    Add files via upload

---
+++ js/image-slider.js
@@ -0,0 +1,27 @@
+var myIndex = 0;
+carousel_images();
+carousel_slides();
+
+function carousel_images() {
+  var i;
+  var x = document.getElementsByClassName("myImages");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 3000); // Change image every 3 seconds
+}
+
+function carousel_slides() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 3000); // Change image every 3 seconds
+}
